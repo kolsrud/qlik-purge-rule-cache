53528493c9708a7b6fe606d81faa247e518e08aa
Øystein Kolsrud <yko@qlik.com>
2018-05-15 09:21:31 +0200

    Initial version.

---
+++ PurgeRuleCache/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace PurgeRuleCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            if (args.Length < 1)
+            {
+                PrintUsage();
+            }
+
+            var url = args[0];
+            Console.WriteLine("Connecting to " + url);
+            var client = new RestClient(url);
+            client.AsNtlmUserViaProxy();
+            try
+            {
+                client.Get("/qrs/about");
+                Console.WriteLine("Connection successfully established.");
+                Console.Write("Creating dummy rule... ");
+                var response = client.Post("/qrs/systemrule", MakeDummyRuleBody());
+                var id = JObject.Parse(response).Property("id").Value.ToString();
+                Console.WriteLine("Rule successfully created, id=" + id);
+                Console.Write("Deleting rule id=" + id + "... ");
+                client.Delete("/qrs/systemrule/" + id);
+                Console.WriteLine("Rule deleted");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Operation failed with message: " + e.Message + " ");
+                throw;
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Usage:   PurgeRuleCache.exe <url>");
+            Console.WriteLine("Example: PurgeRuleCache.exe https://my.server.url");
+            Environment.Exit(1);
+        }
+
+        private static string MakeDummyRuleBody()
+        {
+            return "{"
+                   + "\"category\": \"Security\","
+                   + "\"name\": \"Dummy rule for clearing rule cache\","
+                   + "\"rule\": \"((user.name!=\\\"*\\\"))\","
+                   + "\"actions\": 2,"
+                   + "\"resourceFilter\": \"*\","
+                   + "\"comment\": \"Dummy rule for clearing rule cache\","
+                   + "\"ruleContext\": 0"
+                   + "}";
+        }
+    }
+}
---
+++ PurgeRuleCache/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PurgeRuleCache")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PurgeRuleCache")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("57318c5e-4644-4329-982d-c76c8ec5cd01")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
